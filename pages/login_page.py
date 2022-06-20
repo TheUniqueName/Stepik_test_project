868104d1e4a1903520083db14dbcd08e2e949e19
dkartushin <dkartushin@tradingview.com>
2022-06-20 09:54:20 +0300

    Working on Page Object Model vol.5

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self, browser):
+        substr_position = browser.current_url.find(LoginPageLocators.LOGIN_LINK_SUBSTRING)
+        assert substr_position > -1, f"There is no '{LoginPageLocators.LOGIN_LINK_SUBSTRING}' substring in current URL"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
